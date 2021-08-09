b04d8c309601821cd3c414ae4d623eaf06683b79
Filip Figiel <figiel.filip@gmail.com>
2021-08-10 01:41:22 +0200

    chore: set up elm-review

---
+++ review/src/ReviewConfig.elm
@@ -0,0 +1,62 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+import NoDebug.Log
+import NoDebug.TodoOrToString
+import NoExposingEverything
+import NoImportingEverything
+import NoMissingSubscriptionsCall
+import NoMissingTypeAnnotation
+import NoMissingTypeExpose
+import NoPrematureLetComputation
+import NoRecursiveUpdate
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
+import NoUselessSubscriptions
+import Review.Rule exposing (Rule)
+
+
+config : List Rule
+config =
+    [ -- jfmengels/elm-review-debug
+      NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+
+    -- jfmengels/elm-review-common
+    , NoExposingEverything.rule
+    , NoImportingEverything.rule []
+    , NoMissingTypeAnnotation.rule
+    , NoMissingTypeExpose.rule
+    , NoPrematureLetComputation.rule
+
+    -- jfmengels/elm-review-the-elm-architecture
+    , NoMissingSubscriptionsCall.rule
+    , NoRecursiveUpdate.rule
+    , NoUselessSubscriptions.rule
+
+    -- jfmengels/elm-review-unused
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.Dependencies.rule
+    , NoUnused.Exports.rule
+    , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
+    ]
