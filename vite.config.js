8458a7bb625a34916402f9064664ce374c39c949
Filip Figiel <figiel.filip@gmail.com>
2021-12-30 17:33:27 +0100

    fix: add offline access

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa'
 import elmPlugin from "vite-plugin-elm"
 
 export default defineConfig({
   plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Color tuner",
+        short_name: "Color tuner",
+        display: "standalone",
+        description: "Fine-tune your palette with HSLuv",
+        background_color: '#ffffff',
+      }
+    }),
     elmPlugin({
       // debug: false,
     }),
